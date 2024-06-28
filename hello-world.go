a1bc440288b94b4c48dbe67eac7ae45258a405e0
Lina María Montaño Ramírez - calypsobronte <lina.montano@mercadolibre.com.co>
2024-06-28 13:19:30 -0500

    First hello world program in GO

---
+++ hello-world.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hola, mundo")
+}
+
+// Run: go run hello-world.go
+// First hello world program in Go
+// Lina Maria Montaño Ramírez - calypsobronte
+// 2024-06-28
